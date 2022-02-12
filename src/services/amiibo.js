9fc42e8d6567dc7e70cdf2c2e9a38b001fc79041
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-02-12 12:21:34 -0300

    Create services for api requests(amiibo)

---
+++ src/services/amiibo.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { generateRandomPrice, generateId } from '../helpers'
+
+class Amiibo {
+  constructor(baseUrl='https://amiiboapi.com/api') {
+    this.baseUrl = baseUrl
+    this.service = axios.create({ baseURL: this.baseUrl })
+    this.amiibos = []
+    this.all()
+  }
+
+  async all(opts={}) {
+    try {
+      console.log(opts);
+      const params = new URLSearchParams({ gameseries: 'Super Mario', ...opts})
+      const response = await this.service.get('/amiibo', { params });
+      const { data } = response;
+      const { amiibo } = data
+
+      this.amiibos = this.setData(amiibo)
+      console.log(this.amiibos);
+      return this.amiibos
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  setData(array) {
+    return array.map(amiibo => {
+      return { ...amiibo, id: generateId(), price: generateRandomPrice(1, 30), quantity: 1 }
+    })
+  }
+}
+
+export default Amiibo;
