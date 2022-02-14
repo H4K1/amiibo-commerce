ac45cfb5e2e931e0d72c15e43e3f7d3a81d89ce8
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-02-13 22:39:18 -0300

    Modify initial state for shoppingCart

--- src/redux/shoppingCart/index.js
+++ src/redux/shoppingCart/index.js
@@ -1,6 +1,9 @@
+import localStorage from "../../services/localStorage";
 import { cartAddItem, cartRemoveItem } from "./utils"; 
 
-const shoppingCartReducer = (state = [], action) => {
+const initialState = localStorage.getData('shopping_cart')
+
+const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
     case "cart/addItem":
       return cartAddItem(state, action)
