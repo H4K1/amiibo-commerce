387f9d59e93ba3bb9af7dd0eec4f8c7b2765b8ab
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-02-13 21:30:27 -0300

    Add responsive grids for ProductCard component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,29 @@
-import ProductCard from '../components/ProductCard'
-import Container from '../components/Container'
+import ProductCard from "../components/ProductCard";
+import Container from "../components/Container";
+import { useState, useEffect } from "react";
+import Amiibo from "../services/amiibo";
 
 const Home = () => {
+  const [amiibos, setAmiibos] = useState([]);
+  const service = new Amiibo();
+
+  useEffect(async () => {
+    setAmiibos(await service.all());
+  }, []);
 
   return (
     <main>
-      <Container>
-        <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+      <Container className="flex justify-center">
+        <div className="grid grid-cols-12 gap-2 my-2">
+          {amiibos.map((amiibo, index) => (
+            <div className="col-span-12 md:col-span-3">
+              <ProductCard key={index} amiibo={amiibo} />
+            </div>
+          ))}
         </div>
       </Container>
     </main>
-  )
-}
+  );
+};
 
 export default Home;
