3080d8c88ea8274f5cd69f3ca392c17482377d26
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-02-13 21:23:29 -0300

    Remove inline-block style for Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 const Card = ({ children, ...opts }) => {
   const { className='' } = opts 
   const style = {
-    maxWidth: '16rem'
+    maxWidth: '16rem',
   }
 
   return (
-    <div style={style} className={`inline-block mx-3 my-0 bg-white shadow-lg rounded mb-4 ${className}`}>
+    <div style={style} className={`mx-3 my-0 bg-white shadow-lg rounded mb-4 ${className}`}>
       { children }
     </div>
   )
