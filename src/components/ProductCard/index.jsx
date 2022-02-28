ad7217c10a345c14eea29e71a71be67eb515dcb5
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-02-28 18:36:23 -0300

    Refactor ProductCard component with styles sass

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,7 +1,8 @@
+import './style.sass';
 import Button from './Button';
 import Card from './Card';
 import { addItemToCart } from '../redux/shoppingCart/action';
-import { currencyFormat } from '../helpers'
+import { currencyFormat } from '../helpers';
 import { useDispatch } from 'react-redux';
 import { object } from 'prop-types';
 
@@ -10,16 +11,16 @@ const ProductCard = ({ amiibo }) => {
   const { name, price, image, amiiboSeries } = amiibo;
 
   return (
-    <Card className="">
-      <img className="pt-5 mb-4 object-contain h-48 w-96 " src={image} />
+    <Card className="product">
+      <img className="product--image" src={image} />
 
-      <section className="p-3 pb-4">
-        <div className="font-bold text-sm mb-0 md:text-xl mb-2">{name}</div>
-        <span className="font-thin text-sm">{ amiiboSeries }</span>
-        <p className="font-semibold text-green-500 text-base">{currencyFormat(price)}</p>
+      <section className="product__body">
+        <div className="product__body--title">{name}</div>
+        <span className="product__body--description">{amiiboSeries}</span>
+        <p className="product__body--price">{currencyFormat(price)}</p>
       </section>
 
-      <section className="">
+      <section>
         <Button
           onClick={() => dispatch(addItemToCart(amiibo))}
           className="w-full"
@@ -34,6 +35,6 @@ const ProductCard = ({ amiibo }) => {
 
 ProductCard.propTypes = {
   amiibo: object,
-}
+};
 
 export default ProductCard;
