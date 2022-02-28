04ee7f538d0709a8e12105c21a9fa9f0d014461f
H4K1 <71144997+H4K1@users.noreply.github.com>
2022-02-28 18:50:53 -0300

    Modify double quote to single quotes on .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-     "quotes": ["error", "single"],
-     "react/react-in-jsx-scope": "off",
-     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    }
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'quotes': ['error', 'single'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+  }
 }
